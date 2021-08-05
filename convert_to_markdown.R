be181fe9c26048466f05ec1a2130f03ba1f9e9d3
cnilsen <cnilsen@gmail.com>
2021-08-05 10:34:17 -0700

    fixes tables

--- convert_to_markdown.R
+++ convert_to_markdown.R
@@ -5,7 +5,7 @@ require(rmarkdown)
 file=here::here("documents","2021_07_31 Statistics Tech Memo.docx")
 
 #enter markdown output name and location 
-md_file = here::here("documents","2021_07_31_Statistics_Tech_Memo.md")
+md_file = here::here("documents","2021_07_31_Statistics_Tech_Memo_v2.md")
 
 # render as github flavored markdown (gfm)
 pandoc_convert(file,to="gfm",output = md_file, citeproc = TRUE, options=c("--extract-media=."))
